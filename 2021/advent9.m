c6dbbc5c47b602ee7219ab8132b0ca874ab596db
A-Phamfam <alextampham3000@gmail.com>
2021-12-09 01:11:17 -0600

    2021 day 9

---
+++ 2021/advent9.m
@@ -0,0 +1,40 @@
+%% Part 1
+data = readlines("input9.txt").split("");
+data = double( data(:,2:end-1) );
+sum(data(imregionalmin(data,4))+1)
+
+
+%% Part 2
+data = readlines("input9.txt").split("");
+data = double( data(:,2:end-1) );
+
+basins = bwconncomp(data < 9, 4);
+basinSize = cellfun(@numel,basins.PixelIdxList);
+basinSize = sort(basinSize);
+prod(basinSize(end-2:end))
+
+%% Plots
+figure;
+pcolor(data);
+shading flat
+
+figure;
+pcolor(~(data < 9));
+shading flat;
+
+figure;
+pcolor(~imregionalmin(data,4))
+shading flat;
+
+%% Part 1 Manual
+data = readlines("input9.txt").split("");
+Z = double( data(:,2:end-1) )
+
+Zabove = circshift(Z,1,1);  Zabove(1,:) = 9;
+Zbelow = circshift(Z,-1,1); Zbelow(end,:) = 9;
+Zleft = circshift(Z,1,2);   Zleft(:,1) = 9;
+Zright = circshift(Z,-1,2); Zright(:,end) = 9;
+
+localMin = (Z < Zabove) & (Z < Zbelow) & (Z < Zright) & (Z < Zleft);
+
+sum( Z(localMin)+1 )
